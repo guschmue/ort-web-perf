8c84f9f9dffa6009111802da2e6e7164e4ed3f6e
guschmue <guschmue@microsoft.com>
2024-01-10 12:11:31 -0800

    add --all to add all nodes

--- onnx-add-intermediate-output.py
+++ onnx-add-intermediate-output.py
@@ -6,9 +6,12 @@ def get_args():
     parser = argparse.ArgumentParser(description='onnxruntime bench tool')
     parser.add_argument("--output", required=True, help='output')
     parser.add_argument("--input", required=True, help='input')
-    parser.add_argument("--op", help='op-type to add to output')
-    parser.add_argument("--name", help='node name to add to output')
+    parser.add_argument("--op", default="", help='op-type to add to output')
+    parser.add_argument("--name", default="", help='node name to add to output')
     parser.add_argument("--purge", action='store_true', help="remove original outputs")
+    parser.add_argument("--all", action='store_true', help="add all nodes as output")
+    # parser.add_argument("--unused_nodes", action='store_true', help="remove unused nodes")
+    # parser.add_argument("--unused_const", action='store_true', help="remove unused initializers")
     parser.add_argument("--count", type=int, default=0, help='number of ops to take')
     args = parser.parse_args()
     args.op = args.op.split(",")
@@ -17,8 +20,10 @@ def get_args():
 
 
 def add_output(model, output_name):
+    print(f"adding {output_name}")
     output = model.graph.output.add()
     output.name = output_name
+    print(f"add output {output_name}")
 
 
 def main():
@@ -30,18 +35,17 @@ def main():
         model.graph.ClearField("output")
 
     for i in model.graph.node:
-        if i.op_type in args.op:
-            add_output(model, i.output[0])
-            taken += 1
-            if args.count > 0 and taken >= args.count:
-                break
-        if i.name in args.name:
+        if i.op_type in args.op or i.name in args.name or args.all:
             add_output(model, i.output[0])
             taken += 1
             if args.count > 0 and taken >= args.count:
                 break
 
     if args.output:
+        model_with_shapes = onnx.shape_inference.infer_shapes(model)
+        model.graph.ClearField("output")
+        for i in model_with_shapes.graph.output:
+            model.graph.output.add().CopyFrom(i)
         onnx.save(model, args.output)
         print(f"output in {args.output}")
 
