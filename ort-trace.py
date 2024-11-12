71160723461ecce0f84b0f6797134a841f462125
Guenther Schmuelling <guschmue@microsoft.com>
2024-11-12 08:53:22 -0800

    latest ort-trace.py

--- ort-trace.py
+++ ort-trace.py
@@ -1,3 +1,9 @@
+#
+# tool to analyze onnxruntime trace files
+# for example:
+# python ort-trace.py -skip-first --provider --type-in-name -l 50  trace.json
+#
+
 import argparse
 import json
 import logging
@@ -28,6 +34,8 @@ def get_args():
     parser.add_argument('--type-in-name', action='store_true', help='add dtype to op_type')
     parser.add_argument('--provider', action='store_true', help='group by provider')
     parser.add_argument('--mem', action='store_true', help='sort by memory usage')
+    parser.add_argument('--skip-first', action='store_true', help='skip first inference')
+    parser.add_argument('--pct_kernel', action='store_true', help='use kernel time for pct')
     args = parser.parse_args()
     if args.exclude:
         args.exclude = args.exclude.split(",")
@@ -53,12 +61,31 @@ def json_to_df(profile_path, exclude, webgpu_timestamps, verbose):
 
     if type(data) == dict:
         data = data['traceEvents']
+
+    if not webgpu_timestamps:
+        # make a pass and get webgpu kernel timestamps
+        webgpu_timestamps = {}
+        for item in data:
+            dur = item.get("dur")
+            if dur is None:
+                continue
+            cat = item.get("cat")
+            if cat not in ["Api"]:
+                continue
+            name = item['name']
+            i = name.rfind("_")
+            name = name[:i]
+            webgpu_timestamps[name] = dur
+
     for item in data:
         dur = item.get("dur")
         if dur is None:
             continue
+        ts = item.get("ts")
+        if ts is None:
+            continue
         cat = item.get("cat")
-        if cat not in ["Node", "Op"]:
+        if cat not in ["Node", "Op", "Api"]:
             continue
         arg = item.get('args')
         if not arg:
@@ -75,6 +102,7 @@ def json_to_df(profile_path, exclude, webgpu_timestamps, verbose):
             if exclude and op in exclude:
                 continue
             dur = item['dur']
+            kernel = item['dur']
             parameter_size = float(arg.get('parameter_size'))
             activation_size = float(arg.get('activation_size'))
             output_size = float(arg.get('output_size'))
@@ -85,22 +113,27 @@ def json_to_df(profile_path, exclude, webgpu_timestamps, verbose):
 
             if provider == "Js" and webgpu_timestamps:
                 if op == "MemcpyToHost":
-                    dur -= webgpu_acc
-                    webgpu_acc = 0
+                    # dur -= webgpu_acc
+                    # webgpu_acc = 0
                     pass
                 else:
                     w = webgpu_timestamps.get(name)
                     if w:
-                        # should this be dur += w['time'] ?
-                        dur = w['time']
+                        kernel = w['time']
                         # next line will substract the kernel time from memcopytohost
                         # webgpu_acc += w['time']
                         # print(f"{name}, {op}, {provider} not a kernel")
 
+            if provider == "WebGpu" and webgpu_timestamps:
+                w = webgpu_timestamps.get(name)
+                if w:
+                    kernel = w
+
             e = {
-                "name": name, "dur": dur, "op_type": op, "provider": provider,
+                "name": name, "dur": dur, "kernel": kernel, "op_type": op, "provider": provider,
                 "parameter_size": parameter_size, "activation_size": activation_size,
                 "output_size": output_size, "shape": input_shape, "dtype": input_dtype,
+                "ts": ts
             }
             entries.append(e)
     df = pd.DataFrame([f for f in entries])
@@ -113,9 +146,11 @@ def main():
 
     webgpu_timestamps = None
     if args.webgpu:
-        with open(args.webgpu, "r") as f:
+        with open(args.webgpu, "r", encoding="utf-8") as f:
             webgpu_timestamps = json.load(f)
             webgpu_timestamps = {i["name"]: i for i in webgpu_timestamps}
+
+    # get ops info from chrome trace into a data frame
     df_list = []
     for fname in args.strings:
         try:
@@ -130,13 +165,21 @@ def main():
     df = pd.concat(df_list)
     df_list = None
 
+    # df contains all ops
     digits = 1
     top = args.l
+    if args.skip_first:
+        # if we want to skip the first infererce, trim the data frame
+        first_name = df['name'].iloc[0]
+        second = df.index[df['name'] == first_name][1]
+        df = df[second:]
+
     pd.set_option('display.max_colwidth', 120)
-    df2 = df[['dur', 'count']].sum()
-    df['pct'] = (100 * df['dur'] / df2['dur'])
+    pct_field = "kernel" if args.pct_kernel else "dur"
+    df2 = df[['dur', 'kernel', 'count']].sum()
+    df['pct'] = (100 * df[pct_field] / df2[pct_field])
     mem_field = "output_size"
-    sort_by = mem_field if args.mem else "dur"
+    sort_by = mem_field if args.mem else pct_field
     extra_fields = [mem_field] if args.mem else []
     if args.provider:
         extra_fields.append("provider")
@@ -145,12 +188,14 @@ def main():
     if args.type_in_name:
         df['op_type'] = df['op_type'] + "." + df['dtype']
     if not args.nodes:
-        fields = ["op_type", "dur", "pct", "count", "shape", "dtype"] + extra_fields
+        fields = ["op_type", "dur", "kernel", "pct", "count"] + extra_fields
         groups = ['op_type']
         if args.dtypes:
             groups.append('dtype')
+            fields.append('dtype')
         if args.shapes:
             groups.append('shape')
+            fields.append('shape')
         if args.provider:
             groups.append('provider')
         df1 = df[fields].groupby(groups).sum()
@@ -159,31 +204,28 @@ def main():
         df1 = df1.sort_values(by=sort_by, ascending=False)[:top]
         df1['csum'] = df1['pct'].cumsum()
         df1['avg'] = df1['dur'] / df1['count']
+        df1['avg_kernel'] = df1['kernel'] / df1['count']
         print("\n--Top ops by total runtime")
         print(df1.round(digits).to_string(index=True))
     else:
-        fields = ["name", "op_type", "dur", "pct", "count"] + extra_fields
+        fields = ["name", "op_type", "dur", "kernel", "pct", "count"] + extra_fields
         df1 = df[fields].groupby(['name', "op_type"]).sum()
         if args.mem:
             df1[mem_field] = df1[mem_field] / df1['count'] / 1024 / 1024
         df1 = df1.sort_values(by=sort_by, ascending=False)[:top]
         df1['csum'] = df1['pct'].cumsum()
         df1['avg'] = df1['dur'] / df1['count']
+        df1['avg_kernel'] = df1['kernel'] / df1['count']
         print("\n--Top nodes by total runtime")
         print(df1.round(digits).to_string(index=True))
 
     if args.csv:
         if args.shapes:
             df1 = df1.reset_index().set_index('op_type')
-        if True or args.cov:
-            # impl = {}
-            # with open(args.cov, "r") as f:
-            #    for i in f:
-            #        impl[i.strip()] = 1
-            df1['aggname'] = df1.index.map(lambda x: x[0] + "." + x[1])
-            # df1['aggimpl'] = df1["aggname"].apply(lambda x: impl.get(x, 0))
-            df1['aggsrc'] = os.path.basename(args.strings[0])
-            df1['aggcnt'] = 1
+        df1['aggname'] = df1.index.map(lambda x: x[0] + "." + x[1])
+        # df1['aggimpl'] = df1["aggname"].apply(lambda x: impl.get(x, 0))
+        df1['aggsrc'] = os.path.basename(args.strings[0])
+        df1['aggcnt'] = 1
         df1.to_csv(args.csv)
 
 
